9e3afae0042e096ec4d6e221e64bf2610f500b3d
Viola Bergere <viola.bergere@gmail.com>
2025-02-03 14:39:31 +0000

    style: add extra padding to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 const Footer = () => {
   return (
     <footer className="noise flex text-center z-20 shadow-lg w-full px-3 py-4">
-      <div className="container mx-auto flex flex-col items-center justify-center">
+      <div className="container mx-auto flex flex-col items-center justify-center py-2">
         <p>© {new Date().getFullYear()} Viola Bergere | All rights reserved</p>
         <div className="flex gap-4 mt-2">
           <a
